6975210463123f8aaad14852326071b376c1e307
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2023-05-08 10:48:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const cameraHeight = 550;
 const birdWidth = 34;
 const birdHeight = 24;
 const birdAnimationSpeed = 0.1;
+const groundHeight = 70;
+const groundWidth = cameraWidth;
 
 let app = new PIXI.Application({
   width: cameraWidth,
@@ -32,10 +34,27 @@ for (let i = 0; i < 3; i++) {
 
 const birdAnimation = new PIXI.AnimatedSprite(textureArray);
 birdAnimation.x = Math.floor(cameraWidth / 2 - birdWidth / 2);
-birdAnimation.y = Math.floor(cameraHeight / 2 - birdHeight / 2);
 
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+  elapsed += delta;
+  birdAnimation.y = 50.0 + Math.cos(elapsed/15) * 5.0;
+});
+
+birdAnimation.y = Math.floor(cameraHeight / 2 - birdHeight / 2);
 birdAnimation.animationSpeed = birdAnimationSpeed * 2;
 
 app.stage.addChild(birdAnimation);
 
 birdAnimation.play();
+
+
+const ground = new PIXI.Sprite(
+  PIXI.Texture.from("Image/base.png")
+);
+ground.width = groundWidth;
+ground.height = groundHeight;
+ground.x = 0;
+ground.y = cameraHeight-groundHeight;
+
+app.stage.addChild(ground);
